38ddf308701880fd9b48de5d942665b7b84398a1
Kermout <wm@kermout.com>
2019-11-19 17:52:01 -0800

    Stable for Release

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 var path = require("path")
 
-function createWindow () {
-  // Create the browser window.
+function createWindow() {
+
+  // Create the Browser Window
   let win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png'), // set icon
+    frame: 0,
     webPreferences: {
       nodeIntegration: true
     }
   })
 
-  // and load the index.html of the app.
+
+  // Load Index
   win.loadFile('app/index.html')
 
 }
 
+// Create Window
+Menu.setApplicationMenu(null);
 app.on('ready', createWindow)
+
+// If Closed ... Clean!
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// If Null... Open!
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
