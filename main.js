18973feb088ffc0cc1615525b65d9a2349647117
Kermout <wm@kermout.com>
2019-11-19 17:00:09 -0800

    Fixed Issues

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require('electron')
+var path = require("path")
 
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
     webPreferences: {
       nodeIntegration: true
     }
@@ -12,6 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('app/index.html')
+
 }
 
 app.on('ready', createWindow)
